983d71908a8bddf4b41784e9fbe7a4a7296a350f
Dominic Francis <github@dominicfrancis.co.uk>
2025-05-19 13:42:57 +0100

    refactor: update contact model

--- the_cult_film_club/apps/contact/models.py
+++ the_cult_film_club/apps/contact/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.core.validators import validate_email
 
 
 class ContactUs(models.Model):
-    created = models.DateTimeField(auto_now_add=True, blank=False, null=False)
-    first_name = models.CharField(max_length=30, blank=False, null=False)
-    last_name = models.CharField(max_length=30, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    message = models.TextField(max_length=1000, blank=False, null=False)
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+    email = models.EmailField(
+        validators=[validate_email],
+        blank=False,
+        null=False
+    )
+    message = models.TextField(max_length=1000, blank=True, null=False)
 
     def __str__(self):
         return (

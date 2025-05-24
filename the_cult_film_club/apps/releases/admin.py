407fb028a14461e12879b188e8d02dd6d32a7422
Dominic Francis <github@dominicfrancis.co.uk>
2025-05-24 14:03:34 +0100

    style: remove images as they are stored in cloudinary

--- the_cult_film_club/apps/releases/admin.py
+++ the_cult_film_club/apps/releases/admin.py
@@ -5,19 +5,17 @@ from .models import Releases, Images, Rating
 @admin.register(Releases)
 class ReleaseAdmin(admin.ModelAdmin):
     list_display = (
-        'title', 'release_date', 'director', 'genre', 'subgenre',
-        'resolution', 'resolution', 'edition', 'packaging', 'get_images'
+        'title', 'release_date', 'director', 'genre'
     )
     list_filter = (
-        'title', 'release_date', 'director', 'description', 'genre', 'subgenre',
-        'resolution', 'edition', 'packaging'
+        'genre', 'resolution'
     )
     search_fields = (
         'title', 'release_date', 'director', 'description', 'genre', 'subgenre',
         'resolution', 'special_features', 'edition',
         'censor_status', 'packaging'
     )
-    ordering = ['title', 'release_date', 'director', 'genre', 'subgenre']
+    ordering = ['title', 'release_date', 'director', 'genre']
 
     def get_images(self, obj):
         return ", ".join([str(img) for img in obj.image.all()])
@@ -26,10 +24,10 @@ class ReleaseAdmin(admin.ModelAdmin):
 
 @admin.register(Images)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('image', 'title')
-    list_filter = ('image', 'title')
-    search_fields = ('image', 'title')
-    ordering = ['image', 'title']
+    list_display = ['title']
+    list_filter = ['title']
+    search_fields = ['title']
+    ordering = ['title']
 
 
 @admin.register(Rating)

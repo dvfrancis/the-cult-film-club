8e19354fd4d216f0077e210f2553bb20cbe057d3
Dominic Francis <github@dominicfrancis.co.uk>
2025-05-25 15:25:49 +0100

    refactor: update releases model and releases template

--- the_cult_film_club/apps/releases/models.py
+++ the_cult_film_club/apps/releases/models.py
@@ -25,6 +25,12 @@ class Releases(models.Model):
     censor_status = models.CharField(max_length=500, blank=True, null=True)
     packaging = models.CharField(max_length=500, blank=True, null=True)
     copies_available = models.IntegerField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=False,
+        null=False
+    )
 
     def __str__(self):
         return f"{self.title} ({self.release_date})"

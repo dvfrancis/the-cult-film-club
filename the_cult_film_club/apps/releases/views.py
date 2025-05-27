563dbf6370bfa3656dd00824ddf9f3eba0edc442
Dominic Francis <github@dominicfrancis.co.uk>
2025-05-27 12:53:35 +0100

    feat: enable search

--- the_cult_film_club/apps/releases/views.py
+++ the_cult_film_club/apps/releases/views.py
@@ -1,13 +1,49 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Releases
 
 
 def releases(request):
     """
-    Display all releases.
+    Display all releases, including sort and search queries.
     """
     releases = Releases.objects.all().order_by('title')
-    context = {"releases": releases}
+    query = None
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request,
+                    "You didn't enter a search term!"
+                )
+                context = {
+                    "releases": [],
+                    "search_term": query,
+                }
+                return render(request, "releases/releases.html", context)
+            queries = (
+                Q(image__caption__icontains=query) |
+                Q(title__icontains=query) |
+                Q(release_date__icontains=query) |
+                Q(director__icontains=query) |
+                Q(description__icontains=query) |
+                Q(genre__icontains=query) |
+                Q(subgenre__icontains=query) |
+                Q(resolution__icontains=query) |
+                Q(special_features__icontains=query) |
+                Q(edition__icontains=query) |
+                Q(censor_status__icontains=query) |
+                Q(packaging__icontains=query) |
+                Q(copies_available__icontains=query) |
+                Q(price__icontains=query)
+            )
+            releases = releases.filter(queries)
+    context = {
+            "releases": releases,
+            "search_term": query,
+        }
     return render(request, "releases/releases.html", context)
 
 

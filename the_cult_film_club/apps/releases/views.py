1eb077724e32f7e161365cdfcab6413aa9249dc5
Dominic Francis <github@dominicfrancis.co.uk>
2025-05-28 16:04:53 +0100

    feat: add ability to sort by certain values

--- the_cult_film_club/apps/releases/views.py
+++ the_cult_film_club/apps/releases/views.py
@@ -1,54 +1,103 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, Avg, F, Value
 from .models import Releases
 from django.core.paginator import Paginator
+from django.db.models.functions import Length, Substr, Reverse, StrIndex
 
 
 def releases(request):
-    """
-    Display all releases, including sort and search queries, with pagination.
-    """
-    releases_list = Releases.objects.all().order_by('title')
+    releases_list = Releases.objects.all()
     query = None
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(
-                    request,
-                    "You didn't enter a search term!"
-                )
-                context = {
-                    "releases": [],
-                    "search_term": query,
-                }
-                return render(request, "releases/releases.html", context)
-            queries = (
-                Q(image__caption__icontains=query) |
-                Q(title__icontains=query) |
-                Q(release_date__icontains=query) |
-                Q(director__icontains=query) |
-                Q(description__icontains=query) |
-                Q(genre__icontains=query) |
-                Q(subgenre__icontains=query) |
-                Q(resolution__icontains=query) |
-                Q(special_features__icontains=query) |
-                Q(edition__icontains=query) |
-                Q(censor_status__icontains=query) |
-                Q(packaging__icontains=query) |
-                Q(copies_available__icontains=query) |
-                Q(price__icontains=query)
-            )
-            releases_list = releases_list.filter(queries)
+    sort = None
+    direction = None
 
-    paginator = Paginator(releases_list, 8)  # Show 8 releases per page
+    # Search logic
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(
+                request,
+                "You didn't enter a search term!"
+            )
+            context = {
+                "releases": [],
+                "search_term": query,
+            }
+            return render(request, "releases/releases.html", context)
+        queries = (
+            Q(image__caption__icontains=query) |
+            Q(title__icontains=query) |
+            Q(release_date__icontains=query) |
+            Q(director__icontains=query) |
+            Q(description__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(subgenre__icontains=query) |
+            Q(resolution__icontains=query) |
+            Q(special_features__icontains=query) |
+            Q(edition__icontains=query) |
+            Q(censor_status__icontains=query) |
+            Q(packaging__icontains=query) |
+            Q(copies_available__icontains=query) |
+            Q(price__icontains=query)
+        )
+        releases_list = releases_list.filter(queries)
+
+    # Sorting logic (always runs)
+    if 'sort' in request.GET:
+        sort = request.GET['sort']
+        sortkey = sort
+        if sort == 'price':
+            sortkey = 'price'
+        elif sort == 'director_last':
+            releases_list = releases_list.annotate(
+                rev_dir=Reverse('director'),
+                last_space_pos=StrIndex(Reverse('director'), Value(' ')),
+            ).annotate(
+                last_name=Substr(
+                    'director',
+                    Length('director') - (F('last_space_pos') - 1),
+                    Length('director')
+                )
+            )
+            sortkey = 'last_name'
+        elif sort == 'rating':
+            releases_list = releases_list.annotate(
+                avg_rating=Avg('ratings__rating')
+            )
+            sortkey = 'avg_rating'
+        # Direction
+        if 'direction' in request.GET:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                releases_list = releases_list.order_by(
+                    F(sortkey).desc(nulls_last=True)
+                )
+            else:
+                releases_list = releases_list.order_by(
+                    F(sortkey).asc(nulls_first=True)
+                )
+        else:
+            releases_list = releases_list.order_by(sortkey)
+    else:
+        # Default to alphabetical by title, when no sort is specified
+        releases_list = releases_list.order_by('title')
+
+    paginator = Paginator(releases_list, 8)
     page_number = request.GET.get('page')
     releases = paginator.get_page(page_number)
+
+    querydict = request.GET.copy()
+    if 'page' in querydict:
+        querydict.pop('page')
+    querystring = querydict.urlencode()
 
     context = {
         "releases": releases,
         "search_term": query,
+        "current_sort": sort,
+        "current_direction": direction,
+        "querystring": querystring,
     }
     return render(request, "releases/releases.html", context)
 

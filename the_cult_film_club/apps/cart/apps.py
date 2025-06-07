7c438199b9f146b43642ebf2d317b6bd37e392b4
Dominic Francis <github@dominicfrancis.co.uk>
2025-06-07 08:26:22 +0100

    feat: add and configure django countries

--- the_cult_film_club/apps/cart/apps.py
+++ the_cult_film_club/apps/cart/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class CartConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "the_cult_film_club.apps.cart"
-    verbose_name = "Shopping Cart"
+    verbose_name = "Shop"
 
     def ready(self):
         from the_cult_film_club.apps.cart import signals  # noqa: F401

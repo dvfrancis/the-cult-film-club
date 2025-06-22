e802d5c79f09756544231ac74d05ac65f9cb12a5
Dominic Francis <github@dominicfrancis.co.uk>
2025-06-22 14:29:50 +0100

    style: adjust style and content of index.html

--- the_cult_film_club/apps/home/views.py
+++ the_cult_film_club/apps/home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from the_cult_film_club.apps.releases.models import Releases
 
 
 def index(request):
     """
-    Render the home page of the Cult Film Club.
+    Render the home page of the Cult Film Club with 3 random releases.
     """
-    return render(request, 'home/index.html')
+    releases = Releases.objects.order_by('?')[:3]  # Get 3 random releases
+    return render(request, 'home/index.html', {'releases': releases})

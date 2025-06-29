8147d0bc9c1394ea5a0a409c9b11435c41d954ae
Dominic Francis <github@dominicfrancis.co.uk>
2025-06-29 09:57:01 +0100

    refactor: add tooltip to add address button on the profile page

--- static/base.js
+++ static/base.js
@@ -432,4 +432,10 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
+
+    // Initialize Bootstrap tooltips
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });

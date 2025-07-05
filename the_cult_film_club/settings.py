55d1433309d163cdaf5ae4e7381f5cb28bd444db
Dominic Francis <github@dominicfrancis.co.uk>
2025-07-05 15:50:41 +0100

    refactor: update CKEditor5 settings

--- the_cult_film_club/settings.py
+++ the_cult_film_club/settings.py
@@ -118,14 +118,44 @@ CKEDITOR_5_CONFIGS = {
             ]
         },
         'image': {
-            'toolbar': [
-                'imageStyle:inline',
-                'imageStyle:wrapText',
-                'imageStyle:breakText',
-                '|',
-                'toggleImageCaption',
-                'imageTextAlternative'
+            'toolbar': ['imageTextAlternative']
+        }
+    },
+    'extends': {
+        'toolbar': [
+            'heading', '|', 'bold', 'italic', 'underline', 'strikethrough',
+            '|', 'bulletedList', 'numberedList', '|', 'outdent', 'indent',
+            '|', 'link', 'blockQuote', '|', 'undo', 'redo'
+        ],
+        'heading': {
+            'options': [
+                {
+                    'model': 'paragraph',
+                    'title': 'Paragraph',
+                    'class': 'ck-heading_paragraph'
+                },
+                {
+                    'model': 'heading1',
+                    'view': 'h1',
+                    'title': 'Heading 1',
+                    'class': 'ck-heading_heading1'
+                },
+                {
+                    'model': 'heading2',
+                    'view': 'h2',
+                    'title': 'Heading 2',
+                    'class': 'ck-heading_heading2'
+                },
+                {
+                    'model': 'heading3',
+                    'view': 'h3',
+                    'title': 'Heading 3',
+                    'class': 'ck-heading_heading3'
+                },
             ]
+        },
+        'image': {
+            'toolbar': ['imageTextAlternative']
         }
     }
 }
